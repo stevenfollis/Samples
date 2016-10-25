776eeff8c044886e84f2164ace92d856dcdceb5d
Steven Follis <steven_follis@live.com>
2016-10-25 08:06:48 -0400

    Generalized values

--- CSVProvisioner/config/config.js
+++ CSVProvisioner/config/config.js
@@ -3,16 +3,17 @@ module.exports = {
     auth: {
 
         // Your subscription ID
-        "AZURE_SUBSCRIPTION_ID": "9f4d814b-7085-44ae-8a42-bbae2a5a3f35",
+        "AZURE_SUBSCRIPTION_ID": "00000000-0000-0000-0000-000000000000",
 
         // AzureAD Application Client ID
-        "APPLICATION_ID": "ca93cb9f-350e-47ef-8ca1-dc367ca189a8",
+        "APPLICATION_ID": "00000000-0000-0000-0000-000000000000",
 
         // AzureAD Application Secret/Key
-        "APPLICATION_SECRET": "CGE5qhjiiY4/wyNDU3rw1riG9Nv6FHem6VyrJaRtCwY=",
+        "APPLICATION_SECRET": "0000000000000000000000000000000000000000000=",
 
         // Your AzureAD tenant as a GUID
-        "DOMAIN": "72f988bf-86f1-41af-91ab-2d7cd011db47"
+        "DOMAIN": "00000000-0000-0000-0000-000000000000"
+
     },
 
     parameters: {
